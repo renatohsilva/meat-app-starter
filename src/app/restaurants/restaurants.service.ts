b691c8de420a4ebb79f4ed16cf6a3a4b885d2a1a
renatohsilva <re_hsilva@hotmail.com>
2019-02-03 03:16:19 -0200

    Implementação dos detalhes dos restaurantes

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,44 +1,28 @@
 import { Restaurant } from './restaurant/restaurant.model';
+import { MEAT_API } from '../app.api';
+import { ErrorHandler } from '../app.error-handler';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
+@Injectable()
 export class RestaurantsService {
-    rests: Restaurant[] = [
-        {
-            id: 'bread-bakery',
-            name: 'Bread & Bakery',
-            category: 'Bakery',
-            deliveryEstimate: '25m',
-            rating: 4.9,
-            imagePath: 'assets/img/restaurants/breadbakery.png'
-        },
-        {
-            id: 'burger-house',
-            name: 'Burger House',
-            category: 'Hamburgers',
-            deliveryEstimate: '100m',
-            rating: 3.5,
-            imagePath: 'assets/img/restaurants/burgerhouse.png',
-        },
-        {
-            id: 'coffee-corner',
-            name: 'Coffee Corner',
-            category: 'Coffee Shop',
-            deliveryEstimate: '30-40m',
-            rating: 4.8,
-            imagePath: 'assets/img/restaurants/coffeecorner.png',
-        },
-        {
-            id: 'green-food',
-            name: 'Green Food',
-            category: 'Saudável',
-            deliveryEstimate: '75m',
-            rating: 4.1,
-            imagePath: 'assets/img/restaurants/greenfood.png',
-        },
-    ];
 
-    constructor() { }
+    constructor(private http: Http) { }
 
-    restaurants(): Restaurant[] {
-        return this.rests;
+    restaurants(): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError);
     };
+
+
+
+    restaurantById(id: string): Observable<Restaurant> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError);
+  }
 }
